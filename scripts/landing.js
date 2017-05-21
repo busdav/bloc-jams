07462b52e493626b948223ae7ed3bf61bdf5e88e
busdav <busdav@outlook.com>
2017-05-21 16:51:30 +0100

    assignment 9 dom events: correct version

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,17 +1,19 @@
 var pointsArray = document.getElementsByClassName('point');
 
-var animatePoints = function(pointsArray) {
+var revealPoint = function(point) {
+    point.style.opacity = 1;
+    point.style.transform = "scaleX(1) translateY(0)";
+    point.style.msTransform = "scaleX(1) translateY(0)";
+    point.style.WebkitTransform = "scaleX(1) translateY(0)";
+};
 
-    var revealPoint = function(index) {
-        pointsArray[index].style.opacity = 1;
-        pointsArray[index].style.transform = "scaleX(1) translateY(0)";
-        pointsArray[index].style.msTransform = "scaleX(1) translateY(0)";
-        pointsArray[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-    }
+var animatePoints = function(points) {
 
-    for (var i = 0; i < pointsArray.length; i++) {
-        revealPoint(i);
-    };
+    forEach(points, revealPoint);
+
+    // for (var i = 0; i < pointsArray.length; i++) {
+    //     revealPoint(i);
+    // };
 
 };
 
