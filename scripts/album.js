7f4daa8968de3e1c48a5c050cab24272c6fa3680
busdav <busdav@outlook.com>
2017-06-12 22:22:39 +0100

    assignment 21 jquery seek bars: complete

--- scripts/album.js
+++ scripts/album.js
@@ -34,7 +34,7 @@ var createSongRow = function(songNumber, songName, songLength) {
        '<tr class="album-view-song-item">'
      + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
      + '  <td class="song-item-title">' + songName + '</td>'
-     + '  <td class="song-item-duration">' + songLength + '</td>'
+     + '  <td class="song-item-duration">' + filterTimeCode(songLength) + '</td>'
      + '</tr>'
      ;
 
@@ -136,6 +136,8 @@ var updateSeekBarWhileSongPlays = function() {
             var $seekBar = $('.seek-control .seek-bar');
 
             updateSeekPercentage($seekBar, seekBarFillRatio);
+
+            setCurrentTimeInPlayerBar(this.getTime());
         });
     }
 };
@@ -207,6 +209,7 @@ var updatePlayerBarSong = function() {
     $('.currently-playing .artist-name').text(currentAlbum.artist);
     $('.currently-playing .artist-song-mobile').text(currentSongFromAlbum.title + " - " + currentAlbum.artist);
     $('.main-controls .play-pause').html(playerBarPauseButton);
+    setTotalTimeInPlayerBar(currentSongFromAlbum.duration);
 
 };
 
@@ -266,6 +269,26 @@ var previousSong = function() {
 
     $previousSongNumberCell.html(pauseButtonTemplate);
     $lastSongNumberCell.html(lastSongNumber);
+};
+
+
+var setCurrentTimeInPlayerBar = function(currentTime) {
+  $('.seek-control .current-time').text(filterTimeCode(currentTime));
+};
+
+var setTotalTimeInPlayerBar = function(totalTime) {
+  $('.seek-control .total-time').text(filterTimeCode(totalTime));
+};
+
+var filterTimeCode = function(timeInSeconds) {
+  var floatTimeinSeconds = parseFloat(timeInSeconds);
+  var wholeMinutes = Math.floor(floatTimeinSeconds / 60);
+  var wholeSeconds = Math.floor(floatTimeinSeconds % 60);
+
+  if (wholeSeconds < 10) {
+    return wholeMinutes + ":0" + wholeSeconds;
+  } else
+    return wholeMinutes + ":" + wholeSeconds;
 };
 
 
